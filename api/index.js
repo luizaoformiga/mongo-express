e3548fd48588e8907fed1d0ead5ead87980b0c10
Nome Sobrenome <pereiralimaluizpaulo7@gmail.com>
2021-03-16 10:59:03 -0300

    heroku

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 import 'dotenv/config';
 
-const port = process.env.PORT || 7758;
-// http://localhost:7758
+const port = process.env.PORT || 7560;
+// http://localhost:7659
 app.listen(port, () => console.log(`Server running`));
